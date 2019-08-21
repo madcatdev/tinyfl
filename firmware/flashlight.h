0af8b56012a3de332f399caecd8301cf7deda533
madcatdev <45917916+madcatdev@users.noreply.github.com>
2019-08-21 03:05:38 +0300

    Add files via upload

---
+++ firmware/flashlight.h
@@ -0,0 +1,91 @@
+/*
+TinyFL v0.3_t - Simple AVR (Attiny13a) PWM LED driver
+*********************************************************************************
+Драйвер светодиода типа CREE на Attiny13a, ШИМ 4.7 KHz, N-ch/P-ch транзистор, кнопка без фиксации
+sw version: 19.08.2019 0.3_t, hw version: v3_t
+ 
+FUSES [NON-INVERTED, 0 = programmed, 1 - unprogrammed (attiny13a datasheet, page 104)]
+(E:FF, H:FD, L:6A)
+BODLEVEL1:0 = 10 (BODLEVEL =  1.8V)
+CKSEL1:0 = 10 (internal rc 9.6MHz)
+CKDIV8 = 0 (9.6 / 8 = 1.2MHz)  
+EESAVE = 0 (EEPROM will NOT be erased during programming)
+*/
+
+#ifndef FLASHLIGHT_H_
+#define FLASHLIGHT_H_
+
+#define F_CPU 1200000UL  // Attiny13 1.2MHz / PWM 4.6 KHz
+
+#include <util/delay.h>
+#include <avr/io.h>
+#include <avr/sleep.h>
+#include <avr/interrupt.h>
+
+#define nop __asm__ __volatile__ ("nop");
+#define u8 uint8_t
+#define u16 uint16_t
+
+// Настройки драйвера
+#define RATE_MAX 255 // Макс. яркость (max = 255)
+#define RATE_MIN 0 // Мин. яркость (min = 0)
+#define RATE_REMEMBER // Запоминать текущую яркость на время сна
+#define RATE_NONLINEAR  // Нелинейный режим изменения яркости (гамма-коррекция). RATE_STEP_DEF будет проигнорирован и rate_step расчитан динамически
+#define RATE_STEP_DEF 10 // Шаг изменения яркости
+#define RATE_STEP_LEN 30 // Время 1 шага в мс
+
+#define BAT_CHECK // Проверка заряда батареи
+#define BAT_PERIOD 60000 // Интервал проверки заряда батареи (примерное), мс. Раз в минуту оптимально. uint16
+//#define BAT_PERIOD 1000 // Для калибровки
+#define BAT_WARNING 135 // Напряжение предупреждения о разряде, 3.0 V 
+#define BAT_SHUTDOWN 115 // Напряжение перехода в спящий режим, 2.7 V
+#define BAT_INFO_STEP 3 // Кол-во единиц АЦП на одно мигание при индикации заряда
+
+#define BTN_READ (!((PINB & _BV(1)) >> 1)) // Чтение состояния кнопки на PB1
+#define BTN_DBCLICK // Двойной клик, включение доп.светодиода
+#define BTN_DBCLICK_DELAY 100 // Задержка клика, мс
+#define BTN_DBCLICK_LEN 100 // Длительность клика, мс
+#define BTN_ONOFF_DELAY 250 // Время нажатия кнопки до входа в режим управления яркостью, мс. uint16
+
+#define AUXMODES // Доп режимы, маяк и строб
+#define AUXMODES_DELAY 10 // Задержка до входа в доп.режимы, x*0.125c
+#define STARTSLEEP // Засыпаем после подачи питания на драйвер
+#define STARTBLINKS // Aux led мигает раза при подаче питания
+ 
+// Настройка пинов и режимов работы транзистора
+//#define NCH // N-channel FET (n-канальный полевик)
+#define PCH // P-channel FET (p-канальный полевик)
+#define LOAD_CONNECT DDRB |= 1; // LOAD Pin PB0
+#define LOAD_DISCONNECT DDRB &= ~1;
+#ifdef NCH 
+	#define LOAD_ON PORTB |= 1; // PB0
+	#define LOAD_OFF PORTB &= ~1;
+#endif
+#ifdef PCH 
+	#define LOAD_ON PORTB &= ~1;
+	#define LOAD_OFF PORTB |= 1;
+	#endif
+#define LED_SETPIN DDRB |= _BV(2); // LED2 Pin
+#define LED_ON PORTB |= _BV(2); // ВКЛ LED2
+#define LED_OFF  PORTB &= ~_BV(2); // ВЫКЛ LED2
+#define LED_INV PORTB ^= _BV(2); // Инвертировать LED2
+
+#define PWM_ON TCCR0A |= _BV(COM0A1); // Включаем ШИМ на PB0
+#define PWM_OFF	TCCR0A &= ~_BV(COM0A1); // Отключаем ШИМ на PB0
+
+
+// Прототипы функций
+int main(void);
+void wakeup(void);
+void setup(void);
+void sleep(void);
+u8 bat_getvoltage(void);
+void beacon(void);
+void strobe(void);
+void pwm_setup(void);
+void longpress(void);
+void shortpress(void);
+void bat_check(void);
+
+
+#endif /* FLASHLIGHT_H_ */
