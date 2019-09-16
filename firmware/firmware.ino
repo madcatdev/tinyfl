2930c96be286c75f499a638f57a916287af81a6e
madcatdev <45917916+madcatdev@users.noreply.github.com>
2019-09-16 19:06:56 +0300

    Update firmware.ino

--- firmware/firmware.ino
+++ firmware/firmware.ino
@@ -1,3 +1,4 @@
 // TinyFL v0.3_t - Simple AVR (Attiny13a) PWM LED driver
 // See the .cpp and h. files for source code
+// Select Tools>Board>Attiny13(a) and Tools>Frequency>1.2MHz
 #include "flashlight.h"
