7f625da36a5ac103317463bc02b15179e7b54f18
madcatdev <none@none>
2020-10-29 22:21:57 +0300

    .ino file updated (comments

--- firmware/firmware.ino
+++ firmware/firmware.ino
@@ -1,4 +1,5 @@
-// TinyFL v0.3_t - Simple AVR (Attiny13a) PWM LED driver
-// See the .cpp and h. files for source code
-// Select Tools>Board>Attiny13(a) and Tools>Frequency>1.2MHz
-#include "flashlight.h"
+// TinyFL - Simple AVR (Attiny13a) PWM LED driver
+// See the main.c for source code and main.h for settings.
+// In Arduino IDE, select Tools>Board>Attiny13(a) and Tools>Frequency>1.2MHz.
+// You may need to install Attiny13 package to Arduino.
+#include "main.h"
