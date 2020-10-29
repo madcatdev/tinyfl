3e9675420f357ef4c3f25a497f674ba7f8d9726b
madcatdev <none@none>
2020-10-29 22:24:50 +0300

    migration from .cpp to .c, also some size improvements and optimisations - now fw size is 834B

---
+++ firmware/main.c
@@ -0,0 +1,305 @@
+/*
+ * TinyFL - Simple AVR (Attiny13) PWM LED driver
+ * (c) madcatdev https://github.com/madcatdev
+*/
+
+#include "main.h" // < All settings are here
+
+// Яркость нагрузки, 0 - выключен, 255 - максимальная (ШИМ)
+u8 rate; 
+// Направление изменения яркости нагрузки, 1 - прибавить ШИМ, 0 - убавить ШИМ
+u8 rate_dir = 0; 
+// Массив точек кривой гамма-коррекции, в конце обязательно maxrate
+const u8 rate_step_array[12] = {10,25,50,60,75,80,90,120,130,140,150,RATE_MAX}; 
+// Состояние заднего светодиода LED2
+bool led_state = false;  
+
+// TODO - попробовать bool заменить на u8
+
+
+int main(void)	{
+	setup();
+	
+	// Main LOOP
+	u16 bat_time = 0; 
+	u16 x;
+	
+	while(1)    {
+		
+		if (BTN_READ){ 
+			x = 0;
+			while ( (BTN_READ) && (x++ < BTN_ONOFF_DELAY) )  // Посчитаем, сколько времени нажата кнопка. 
+				_delay_ms(1);
+			
+			if  (x < BTN_ONOFF_DELAY)  // Короткое нажатие - обрабатываем двойной клик и(или) выключаем МК
+				shortpress();
+			
+			if (x == BTN_ONOFF_DELAY)  // Длинное нажатие - управление яркостью и доп.режимы
+				longpress();
+			
+			while (BTN_READ); // Антидребезг (иначе будут самопроизвольные выключения)
+		}
+
+		#ifdef BAT_CHECK
+		if (bat_time == BAT_CHECK_PERIOD)  { // Проверка заряда батареи через определенный интервал
+			bat_time = 0; 
+			bat_check();
+		}
+		bat_time++;
+		#endif
+		
+		_delay_ms(1);
+	}
+}
+
+ISR(INT0_vect) {}	// Прерывание ничего не делает, кроме пробуждения МК
+ 
+void setup(void){
+	// Настройка МК после подачи питания
+
+	// Настройка АЦП. Attiny13 datasheet, page 92
+	ADMUX = _BV(MUX1) | // [1:0] = 1:0 (ADC2)
+	_BV(ADLAR) | // Отсекаем 2 младших бита, получаем 8-битное разрешение
+	_BV(REFS0); // Внутренний ИОН 1.1V
+	ACSR |= _BV(ACD); // Отключаем компаратор (по умолчанию включен)
+
+	// Настройка таймера. Важно это делать до настройки пина LED_MAIN на выход (иначе видимо не успевает зарядиться C1 после подачи питания)
+	// На OC0B висит INT0, поэтому использовать второй канал (например для AUX_LED) не получится.
+	TCCR0B = _BV(CS00 ); // 001 - тактовый генератор CLK/1 (скорость шим = CLK/256 = 4687.5 Гц)
+	TCCR0A = _BV(WGM01) | _BV(WGM00); // 11 - Режим Fast PWM
+	
+	//TIMSK0 |= _BV(TOIE0); // Прерывание по переполнению
+	
+	// PB0 - LED_MAIN (OC0A шим-канал, транзистор), изначально ВЫКЛ
+	// PB1 - Кнопка POWER/ADJ, оно же INT0, Flip-Flop. Внутр.подтяжка к питанию
+	// PB2 - LED_BACK,  задний светодиод (опционально)
+	// PB3 - не исп. Внутр.подтяжка к питанию (чтобы не болталась в воздухе и не мешала сну)
+	// PB4 - ADC2 (контроль заряда батареи), Hi-z
+	// PB5 - RESET
+	PORTB = _BV(1) | _BV(3);
+	DDRB = _BV (0) | _BV(2);
+	
+	#ifdef STARTBLINKS
+	LED_BACK_on;
+	_delay_ms(50);
+	LED_BACK_off;
+	#endif
+	
+	#ifdef STARTSLEEP
+	sleep();
+	#else
+	wakeup();
+	#endif
+}
+ 
+void wakeup(void) {
+	// Инициализация МК после сна или при старте
+	//rate_dir = 0;
+	if (led_state) 
+		LED_BACK_on;
+	
+	#ifndef RATE_REMEMBER // Если нет памяти яркости
+		rate = RATE_DEFAULT; // включается сразу на мин. яркость
+	#endif
+	OCR0A = rate;
+	
+	ADCSRA |= _BV(ADPS1) | _BV(ADPS0) | _BV(ADEN) ; // Предделитель частоты, включение АЦП
+	ADCSRA |= _BV(ADSC); // Пробная конверсия (необходимо провести первый раз после включения для стабилизации АЦП)
+	
+	while (BTN_READ); // Антидребезг, иначе при включении может сразу выключиться, актуально с некоторыми кнопками
+	PWM_on;
+	_delay_ms(100);
+}
+
+void sleep(void){
+	// Сон в выключенном состоянии
+	PWM_off;
+	LED_MAIN_off;
+	LED_BACK_off;
+	ADCSRA &= ~_BV(ADEN); // Отключаем АЦП
+	
+	do {
+		// Настройка параметров сна
+		GIMSK = _BV(INT0); // ВКЛ прерывание INT0
+		MCUCR = _BV(SE) | _BV(SM1); // SE - SLeep_enable = 1, SM - Power-down = 10, ISC = 00 (low level of INT0 generates interrupt)
+		// Можно делать иначе (но будет на 10 байт больше) - set_sleep_mode(SLEEP_MODE_PWR_DOWN); sleep_enable();
+		
+		sei(); // Включаем прерывания, иначе заснем навсегда
+		asm ("sleep");  // Собственно сон - sleep_cpu();
+		cli(); // Проснулись, чифирнулись, отключили прерывания
+		GIMSK = 0x00; // Отключаем INT0
+		MCUCR = 0x00; // Убираем бит SE, а заодно все остальное. Можно так же и sleep_disable();, но будет на 4 байта больше
+		sei();
+		
+		// Проверка на длительность импульса включения (защита от наносекундных помех)
+		_delay_ms(STARTDELAY);
+		
+	} while (!BTN_READ);
+
+	wakeup(); // Настроим МК и включим периферию
+}
+
+void beacon(void) {
+	// Доп. режим - маячок 1 Гц. В этом режиме нет проверки заряда батареи.
+	OCR0A = RATE_MAX; 
+	while (!BTN_READ) {	// Пока не нажата кнопка, работаем в режиме маяка	
+		PWM_on;
+		_delay_ms(50);
+		PWM_off;
+		_delay_ms(950);		
+	}			
+}
+
+void strobe(void) {
+	// Доп. режим - стробоскоп 8 Гц. В этом режиме нет проверки заряда батареи.		
+	OCR0A = RATE_MAX; 
+	while (!BTN_READ) { // Пока не нажата кнопка, работаем в режиме стробоскопа
+		PWM_on;
+		_delay_ms(25);
+		PWM_off;
+		_delay_ms(100);
+	}		
+}
+
+void longpress(void) {
+	// Длинное нажатие кнопки - управление заполнением ШИМ
+	
+	u8 rate_step = RATE_STEP_DEF;
+	u8 x = 0; // Счетчик циклов для перехода в доп.режим
+	rate_dir =~rate_dir; // Инвертируем направление (больше/меньше % ШИМ)
+
+	while (BTN_READ) { // Пока кнопка нажата, изменяем значение rate 
+		_delay_ms(RATE_STEP_LEN);
+		// Определим rate_step в зависимости от текущего rate
+		// Это сделано для компенсации нелинейной зависимости яркости СД от значения ШИМ (гамма-коррекция)
+		// Подробнее про это можно глянуть тут - youtube.com/watch?v=9zrobCFeeGw (в tinyfl реализация проще)
+		#ifdef RATE_NONLINEAR 
+		// Переберем массив значений кривой step_array, пока не будет совпадения с rate
+		// Массив обязательно должен заканчиваться на maxrate, иначе будет ошибка
+		u8 y = 0;
+		while (rate > rate_step_array[y])  
+			y++;
+		rate_step = y + 1; // Установим rate_step как номер совпавшего элемента массива кривой + 1
+		#endif
+	
+		if (rate_dir) {	// Изменим rate в большую сторону
+			if (rate < RATE_MAX) {
+				if (255 - rate > rate_step)
+					rate += rate_step;
+				else
+					rate = RATE_MAX;
+			}
+		}
+		else {	// Или в меньшую сторону
+			if (rate > RATE_MIN) {
+				if (rate > rate_step)
+					rate -= rate_step;
+				else
+					rate = RATE_MIN;
+			}
+		}
+	
+		// Мигаем СД-CREE, когда достигнуто крайнее значение, отслеживаем переход в доп.режим
+		if ( (rate <= RATE_MIN) || (rate >= RATE_MAX) ) {
+			PWM_off;
+			_delay_ms(125);
+			PWM_on;
+			_delay_ms(125);
+			x++;
+		}	
+	
+		// Входим в подрограмму доп.режима, если прошла активация (125+125)*10 = 2500мс удержания
+		if (x > AUXMODES_DELAY) {
+			#ifdef AUXMODES		
+			while (BTN_READ); 
+			_delay_ms(250); 
+			PWM_off;
+			LED_BACK_off;
+				
+			if (rate <= RATE_MIN)
+				beacon();
+			if (rate >= RATE_MAX)
+				strobe();
+				
+			if (led_state) LED_BACK_on;
+			PWM_on;
+			x = 0;
+			//break;	
+			#endif			
+		}
+		OCR0A = rate; // В итоге настроим ШИМ с новым значением rate	
+	} // END OF while (BTN_READ)
+	
+}
+
+void shortpress(void) {
+	// Короткое нажатие кнопки
+	#ifdef BTN_DBCLICK
+	PWM_off;
+	LED_BACK_off;
+	_delay_ms(BTN_DBCLICK_DELAY);
+	
+	u8 y = 0;
+	while ((y++ < BTN_DBCLICK_LEN) && (!BTN_READ))  // Ждем клик в течении определенного интервала
+		_delay_ms(1);
+			
+	if 	(BTN_READ) { // Если он был, включаем доп.СД (или выключаем)
+		led_state = !led_state;
+		if (led_state) 
+			LED_BACK_on;
+		PWM_on;
+		_delay_ms(100);
+	} else 
+		sleep();
+	#else
+	sleep(); // Иначе просто засыпаем
+	#endif
+
+}
+
+u8 bat_getvoltage(void) {
+	/*
+	Получение напряжения батареи в единицах АЦП
+	Внутренний ИОН 1.1V (1.0-1.2V, page 121). Поэтому значение Vref для расчета делителя стоит принять за 1.0В
+	Макс. напряжение, которое будет в схеме - 4.35В, поэтому Kdiv должен быть в районе 4.4-5.0
+	Для этого подходит пара 750K:220K, Kdel получается 4.41, Vmax = 4.41 В, Vraw = 17.22 mv/adc
+	
+	Формула расчета напряжения из ацп: ADC = (Vin/Kdiv) * 256
+	Коэфициент Vraw mv/raw = (Vion/256)*Kdiv
+	
+	Необходимо делать одну конверсию после включения для "прогрева" АЦП
+	*/
+	//PWM_off;
+	//LED_BACK_off;
+	_delay_us(500); // Стабилизируем напряжение после отключения мощной нагрузки, Занимает 12 байт
+	ADCSRA |= _BV(ADSC); // Начинаем преобразование
+	while (ADCSRA & _BV(ADSC)); // ждем окончания преобразования
+	//PWM_on;
+	//if (led_state) 
+	//	LED_BACK_on;
+	return ADCH;
+}
+
+void bat_check(void) {
+	// Проверка заряда батареи
+	PWM_off;
+	u8 adc_raw = bat_getvoltage();
+	PWM_on;
+
+	// Предупреждение 
+	if (adc_raw < BAT_WARNING) { 
+		u8 x = adc_raw; // Копия значения для отработки мигания
+		while (x < BAT_WARNING) { // Каждое мигание означает 5 позиций АЦП (1 мигание - слабо разряжено, 1+n - сильнее)
+			PWM_off;
+			_delay_ms(50);
+			PWM_on;
+			_delay_ms(50);
+			x += BAT_INFO_STEP;
+		}
+	}
+	
+	// Отключение
+	if (adc_raw < BAT_SHUTDOWN) 
+		sleep();	
+	
+}
---
+++ firmware/main.h
@@ -0,0 +1,136 @@
+/*
+ * TinyFL - Simple AVR (Attiny13) PWM LED driver
+ * MCU: Attiny13(a)
+ * Last version: 20.10.2020 (0.5)
+ * (c) madcatdev https://github.com/madcatdev
+ *---------
+ * Драйвер светодиода типа CREE на Attiny13a, ШИМ 4.7 KHz, N-ch/P-ch транзистор, кнопка без фиксации
+*/
+
+
+#ifndef MAIN_H_
+#define MAIN_H_
+
+// Frequency definition for gcc. Do not forget to set proper fuses.
+// Определение частоты для компилятора. Не забудь выставить частоту фьюзами. 
+#define F_CPU 1200000UL  // Attiny13 1.2MHz / PWM 4.6 KHz / CKDIV8 = 0
+//#define F_CPU 9600000UL  // Attiny13 9.6MHz / PWM 36.8 KHz / CKDIV8 = 1
+
+#include <util/delay.h>
+#include <avr/io.h>
+#include <avr/sleep.h>
+#include <avr/interrupt.h>
+#include <stdbool.h> // bool in C99
+
+#define nop __asm__ __volatile__ ("nop");
+typedef uint8_t u8;
+typedef uint16_t u16;
+
+//-------------------------------------
+
+// Driver settings 
+// Настройки драйвера
+
+// MAX brightness (max = 255) 
+// Макс. яркость
+#define RATE_MAX 250 
+// MIN brightness (min = 0)	
+// Мин. яркость
+#define RATE_MIN 0 
+// Restore brightness level when wakin up 
+// Запоминать текущую яркость на время сна
+#define RATE_REMEMBER 
+// Non-linear brightness mode (gamma-correction). RATE_STEP_DEF will be ignores and rate_step will be calculated on the go
+// Нелинейный режим изменения яркости (гамма-коррекция). RATE_STEP_DEF будет проигнорирован и rate_step расчитан динамически
+#define RATE_NONLINEAR  
+// Brightness adjustment step
+// Шаг изменения яркости
+#define RATE_STEP_DEF 10 
+// Length of brightness adjustment step (ms)
+// Длительность одного шага изменения яркости (мс)
+#define RATE_STEP_LEN 30 
+// Default brightness (when battery connected, or when each wakeup, if RATE_REMEMBER is not defined)
+// Яркость по умолчанию (при первом включении, если не объявлен RATE_REMEMBER - при каждом включении)
+#define RATE_DEFAULT RATE_MIN 
+// Battery voltage check
+// Проверка заряда батареи
+#define BAT_CHECK 
+// Battery voltage check interval (ms). Once per minute is optimal for me. Maximum is 65s (65535)
+// Интервал проверки заряда батареи (примерное), мс. Раз в минуту оптимально. 65с максимально (65535)
+//#define BAT_PERIOD 60000 
+#define BAT_CHECK_PERIOD 1000 // For easy calibration | Для калибровки
+// Battery warning voltage, 135 is about 3.0V
+// Напряжение предупреждения о разряде, 135 ~ 3.0 V
+#define BAT_WARNING 195 
+// Shutdown voltage, 115 is about 2.7V
+// Напряжение перехода в спящий режим, 115 ~ 2.7 V
+#define BAT_SHUTDOWN 180 
+// ADC raw units per one warning blink (when battery is low)
+// Кол-во единиц АЦП на одно мигание при индикации заряда
+#define BAT_INFO_STEP 3 
+// Button reading on PB1
+// Чтение состояния кнопки на PB1
+#define BTN_READ (!(PINB & _BV(1))) 
+// Double click turns on LED_BACK 
+// Двойной клик, включение доп.светодиода
+#define BTN_DBCLICK 
+// Double click delay, ms
+// Задержка клика, мс
+#define BTN_DBCLICK_DELAY 100 
+// Double click length, ms
+// Длительность клика, мс
+#define BTN_DBCLICK_LEN 100 
+// Time between button pressed and sleep mode, ms.
+// Время нажатия кнопки до входа в режим управления яркостью, мс. uint16
+#define BTN_ONOFF_DELAY 250 
+// Additional modes, beacon and strobe
+// Доп режимы, маяк и строб
+#define AUXMODES 
+// Delay beetween entering these modes, x*0.125s
+// Задержка до входа в доп.режимы, x*0.125c
+#define AUXMODES_DELAY 10 
+// Sleep after battery connected for the first time
+// Засыпаем после подачи питания на драйвер
+#define STARTSLEEP 
+// Indication when battery connected for the first time (wery useful sometimes)
+// Индикация подачи питания при помощи LED_BACK (удобно при прошивке)
+#define STARTBLINKS 
+// Turn-on delay, ms. For EMP immunity value between 5-10 is recomended 
+// Задержка при включении, мс. Для помехозащиты рекомендуется 5-10
+#define STARTDELAY 5 
+
+// Peripheral setup
+// Настройка пинов, типа транзистора и таймеров
+//#define NCH // N-channel FET | N-канальный полевик
+#define PCH // P-channel FET | P-канальный полевик)
+#ifdef NCH
+	#define LED_MAIN_on PORTB |= 1; // PB0
+	#define LED_MAIN_off PORTB &= ~1;
+	// 10 - установка 0 на выводе OC0A при совпадении с A, установка 1 на выводе OC0A при обнулении счётчика (неинверсный режим)
+	#define PWM_on TCCR0A |= _BV(COM0A1);
+	#define PWM_off	TCCR0A &= ~_BV(COM0A1); // Отключаем ШИМ на PB0
+#endif
+#ifdef PCH
+	#define LED_MAIN_on PORTB &= ~1;
+	#define LED_MAIN_off PORTB |= 1;
+	// 11 - установка 1 на выводе OC0A при совпадении с A, установка 0 на выводе OC0A при обнулении счётчика (инверсный режим)
+	#define PWM_on TCCR0A |= _BV(COM0A1) | _BV(COM0A0);
+	#define PWM_off	TCCR0A &= ~(_BV(COM0A1) | _BV(COM0A0));
+#endif
+#define LED_BACK_on PORTB |= _BV(2); // ВКЛ LED2
+#define LED_BACK_off  PORTB &= ~_BV(2); // ВЫКЛ LED2
+#define LED_BACK_inv PORTB ^= _BV(2); // Инвертировать LED2
+
+
+int main(void);
+static void setup(void);
+static void wakeup(void);
+void sleep(void);
+static void beacon(void);
+static void strobe(void);
+void longpress(void);
+void shortpress(void);
+static u8 bat_getvoltage(void);
+static void bat_check(void);
+
+#endif /* MAIN_H_ */
