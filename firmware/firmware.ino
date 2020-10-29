f3ff75bd4539179e5c96377001765bfe1272a57f
madcatdev <none@none>
2020-10-29 22:36:49 +0300

    firmware.ino comments

--- firmware/firmware.ino
+++ firmware/firmware.ino
@@ -1,5 +1,12 @@
-// TinyFL - Simple AVR (Attiny13a) PWM LED driver
-// See the main.c for source code and main.h for settings.
-// In Arduino IDE, select Tools>Board>Attiny13(a) and Tools>Frequency>1.2MHz.
-// You may need to install Attiny13 package to Arduino.
+/*
+ TinyFL - Simple AVR (Attiny13a) PWM LED driver
+ See the main.c for source code and main.h for settings.
+ In Arduino IDE, select Tools>Board>Attiny13(a) and Tools>Frequency>1.2MHz.
+ You may need to install Attiny13 package (core) to Arduino.
+ 
+ TinyFL - простой ШИМ-драйвер светодиода для Attiny13
+ Исходный код находится в main.c, а настройки в main.h
+ Для компиляции через Arduino IDE, выберите Tools>Board>Attiny13(a) и Tools>Frequency>1.2MHz
+ Возможно, для корректной работы потребуется установка пакета (core) Attiny13.
+*/
 #include "main.h"
